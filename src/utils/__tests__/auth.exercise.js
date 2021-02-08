9881541435a0bc10f0495cd1b6b0d2decc35974a
Uberto <dev@uberto.me>
2021-02-08 15:10:14 +0000

    test: auth basic

--- src/utils/__tests__/auth.exercise.js
+++ src/utils/__tests__/auth.exercise.js
@@ -1,10 +1,7 @@
 // Testing Pure Functions
 
-// 💣 remove this todo test (it's only here so you don't get an error about missing tests)
-test.todo('remove me')
-
 // 🐨 import the function that we're testing
-// 💰 import {isPasswordAllowed} from '../auth'
+import {isPasswordAllowed} from '../auth'
 
 // 🐨 write tests for valid and invalid passwords
 // 💰 here are some you can use:
@@ -19,3 +16,59 @@ test.todo('remove me')
 // - abc123! // no uppercase letters
 // - ABC123! // no lowercase letters
 // - ABCdef123 // no non-alphanumeric characters
+
+test('password is valid', () => {
+  const password = '!aBc123'
+
+  const result = isPasswordAllowed(password)
+
+  expect(result).toBe(true)
+})
+
+test('password is too short', () => {
+  const password = 'a2c!'
+
+  const result = isPasswordAllowed(password)
+
+  expect(result).toBe(false)
+})
+
+test('password missing alphabet characters', () => {
+  const password = '123456!!'
+
+  const result = isPasswordAllowed(password)
+
+  expect(result).toBe(false)
+})
+
+test('password missing numbers', () => {
+  const password = 'ABCdef!'
+
+  const result = isPasswordAllowed(password)
+
+  expect(result).toBe(false)
+})
+
+test('password missing uppercase letters', () => {
+  const password = 'abc123!!'
+
+  const result = isPasswordAllowed(password)
+
+  expect(result).toBe(false)
+})
+
+test('password missing lowercase letters', () => {
+  const password = 'ABC123!!'
+
+  const result = isPasswordAllowed(password)
+
+  expect(result).toBe(false)
+})
+
+test('password missing non-alphanumeric characters', () => {
+  const password = 'ABCdef123'
+
+  const result = isPasswordAllowed(password)
+
+  expect(result).toBe(false)
+})
